91219483cacc366e27aee6b1801493ad79a922e1
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-09-04 11:31:12 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 3357 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,59 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+
+
+
+  let imageData = []
+
+  fetch(`https://randopic.herokuapp.com/images/${imageId}`)
+  .then(function (response) {
+    return response.json()
+  })
+  .then(imgData => {
+    image = imgData
+    console.log(image)
+
+    // add image to page
+    // let source = document.getElementById("image").src
+    // source.innerHTML = `<img src=${image.url}>`
+
+  })
+
+
+  // likes
+  document.getElementById("like_button").addEventListener("click", function(){
+    console.log("clicked")
+
+    // get likes
+    const likeSpan = document.querySelector("#likes")
+
+    let numLikes = parseInt(likeSpan.innerText)
+
+    // debugger
+    numLikes++
+    likeSpan.innerText = numLikes
+
+
+    // post
+     
+
+
+  });
+
+  // comments
+  document.getElementById("comment_form").addEventListener("submit", e => {
+    e.preventDefault()
+
+    const content = e.target.comment.value
+
+    const list = document.querySelector("#comments")
+    list.insertAdjacentHTML("beforeend", `<li>${content}</li>`)
+    
+
+    // post
+  })
+  
+
 })
+
